168c74cf1e5e60a048f83ebb8364522c6254f9ae
akishoreS <aryasingh0127@gmail.com>
2024-06-24 18:58:31 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home'; // Ensure you have a Home component
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
 import CouponForm from './components/CouponForm';
-import Logout from './components/Logout'; // Ensure you have a Logout component
+import Logout from './components/Logout';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<CouponForm />} />
-            <Route path="/coupons" element={<Home />} />
+            {/* Set CouponForm as the default route */}
+            <Route path="/" element={<Navigate to="/coupons" />} />
+            <Route path="/coupons" element={<CouponForm />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/logout" element={<Logout />} />
         </Routes>
     );
